ed22481d1177be5e62ea36593761e66244d8b26a
Kennethso <kennethso1211@gmail.com>
2023-06-12 18:00:27 +0800

    Update model_factories.py
    Update model_factories

--- bioweb/genedata/model_factories.py
+++ bioweb/genedata/model_factories.py
@@ -1,3 +1,4 @@
+from random import randint, choice
 import factory
 
 from django.test import TestCase
@@ -21,10 +22,16 @@ class SequencingFactory(factory.django.DjangoModelFactory):
 
 
 class GeneFactory(factory.django.DjangoModelFactory):
-    gene_id = "GeneX"
-    entity = "Plasmid"
-    start = 12
-    stop = 100
+    # gene_id = "GeneX"
+    # entity = "Plasmid"
+    # start = 12
+    # stop = 100
+    gene_id = factory.Sequence(lambda n: 'gene%d' % n+str(1))
+    # entity = factory.Faker('sentence', nb_words=1)
+    entity = choice(['Plasmid', 'Chromosome'])
+    start = randint(1, 100000)
+    stop = start+randint(1, 100000)
+
     sense = "+"
     start_codon = "M"
     sequencing = factory.SubFactory(SequencingFactory)
