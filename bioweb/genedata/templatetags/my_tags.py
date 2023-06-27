e46ed3073e852d07c6f625bfb5ff0502db0cefb5
kennethso <kengame121101@gmail.com>
2023-06-27 22:01:31 +0800

    Minor Update components
    update componenets
    apply custom tags

--- bioweb/genedata/templatetags/my_tags.py
+++ bioweb/genedata/templatetags/my_tags.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.simple_tag
 def todays_date():
     return datetime.datetime.now().strftime("%d %b, %Y")
+
+@register.simple_tag
+def genes_count(master_genes):
+    return master_genes.count()
